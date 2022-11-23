03abf4c995c138d2518d62dfe08af75225ec8219
MohamedHamel <mohamed.hamel@minfin.fed.be>
2022-11-23 14:57:15 +0100

    first commit

---
+++ api-bk/src/main/java/hs/mgn/apibk/ApiBkApplication.java
@@ -0,0 +1,13 @@
+package hs.mgn.apibk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBkApplication.class, args);
+	}
+
+}
---
+++ api-bk/src/test/java/hs/mgn/apibk/ApiBkApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.mgn.apibk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
